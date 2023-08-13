905eb928b9b63a779db29149824193d16ed05a63
Andrew <rostangeles1993@gmail.com>
2023-08-13 21:14:28 +0300

    New commit

--- mysql2.php
+++ mysql2.php
@@ -19,7 +19,7 @@ echo "<h1>Создание БД</h1>";
 
 // // Создание БД
 
-// $sql ="CREAT DATABASE DataBase";
+// $sql ="CREATE DATABASE DataBase";
 
 // if ($conn->query($sql) === TRUE) {
 //     echo "Database created successfully!";
@@ -31,29 +31,47 @@ echo "<h1>Создание БД</h1>";
 
 // 2 вариант (процедурный)
 
-$server_name = "Localhost"; // Создаем переменные
+// $server_name = "Localhost"; // Создаем переменные
+// $username = "root";
+// $password = "";
+
+// $conn = mysqli_connect($server_name, $username, $password);
+// // Пробрасываем их в переменную с mysqli
+
+// if ($conn->connect_error) {
+//     die ("Connection failed" . mysqli_connect_error()); // Сообщение об ошибке
+// // Создаем условие, если происходит ошибка
+// }
+
+
+
+
+// // Создание БД
+
+// $sql = "CREATE DATABASE testDB";
+
+// if (mysqli_query($conn, $sql)) {
+//     echo "Database created successfully!";
+// } else {
+//     echo "Error" . mysqli_error($conn);
+// }
+
+// // Закрыть соединение
+
+// mysqli_close($conn);
+
+$server_name = "Localhost";
 $username = "root";
 $password = "";
 
-$conn = mysqli_connect($server_name, $username, $password);
-// Пробрасываем их в переменную с mysqli
+try {
+    $conn = new PDO("mysql:host=$servername; dbname = testDB", $username, $password);
 
-if ($conn->connect_error) {
-    die ("Connection failed" . mysqli_connect_error()); // Сообщение об ошибке
-// Создаем условие, если происходит ошибка
-}
-
-
-
-
-// Создание БД
-
-$sql ="CREAT DATABASE DataBase";
-
-if (mysqli_query($conn, $sql)) {
+    $sql = "CREATE DATABASE testBD2";
+    $conn->exec($sql);
     echo "Database created successfully!";
-} else {
-    echo "Error" . mysqli_error($conn);
+} catch (PDOException $e) {
+    echo $sql . $e->getMassege();
 }
 
 
